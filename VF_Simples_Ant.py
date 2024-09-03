78ec26b18b9e8816cf93c08c1a9235f9d901f480
BrunoXNR <126311403+BrunoXNR@users.noreply.github.com>
2024-09-03 16:21:44 -0300

    Add files via upload

---
+++ VF_Simples_Ant.py
@@ -0,0 +1,78 @@
+# Projeção de Valor Futuro - Parcelas Antecipadas
+
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+# Input das variáveis do cálculo
+capital_inicial = int(input("Digite o valor inicial: "))
+taxa_juros = float(input("Digite a taxa de juros ao mês: "))
+prazo = int(input("Digite o prazo total em meses: "))
+aporte = int(input("Digite o valor dos aportes mensais: "))
+
+taxa_juros = taxa_juros / 100
+
+
+# Cálculo do Valor Futuro com Aportes
+def valor_futuro_com_aporte(capital_inicial, taxa_juros, prazo, aporte):
+    patrimonio_mensal = []
+    valor_futuro_total = capital_inicial
+
+    for mes in range(1, prazo + 1):
+        valor_futuro_total = (valor_futuro_total + aporte) * (1 + taxa_juros)
+        patrimonio_mensal.append(valor_futuro_total)
+
+    return valor_futuro_total, patrimonio_mensal
+
+
+# Cálculo do Valor Futuro sem Aportes
+def valor_futuro_sem_aporte(capital_inicial, taxa_juros, prazo):
+    patrimonio_mensal_sem_aporte = []
+    
+    for mes in range(1, prazo + 1):
+        valor_futuro = capital_inicial * (1 + taxa_juros) ** mes
+        patrimonio_mensal_sem_aporte.append(valor_futuro)
+    
+    return patrimonio_mensal_sem_aporte
+
+
+# Chamada das Funções
+valor_futuro_final_com_aporte, patrimonio_mensal = valor_futuro_com_aporte(capital_inicial, taxa_juros, prazo, aporte)
+patrimonio_mensal_sem_aporte = valor_futuro_sem_aporte(capital_inicial, taxa_juros, prazo)
+
+# Retornar o  valor final da projeção com aportes
+print(f"Valor Futuro com Aporte: R$ {valor_futuro_final_com_aporte:.2f}")
+
+# Calcular e retornar o valor final da projeção sem aportes
+valor_futuro_final_sem_aporte = patrimonio_mensal_sem_aporte[-1]
+print(f"Valor Futuro sem Aporte: R$ {valor_futuro_final_sem_aporte:.2f}")
+
+
+
+# Criando o DataFrame para plotagem
+df = pd.DataFrame({
+    'Mês': np.arange(1, prazo + 1),
+    'Patrimônio com Aporte': patrimonio_mensal,
+    'Patrimônio sem Aporte': patrimonio_mensal_sem_aporte
+})
+
+
+# Plotagem do Gráfico
+sns.set_theme(style="whitegrid")
+
+plt.figure(figsize=(12, 6))
+sns.lineplot(x='Mês', y='Patrimônio com Aporte', data=df, marker='o', color='b', label='Com Aporte')
+sns.lineplot(x='Mês', y='Patrimônio sem Aporte', data=df, marker='o', color='r', label='Sem Aporte')
+
+plt.title('Patrimônio Projetado (Parcelas Antecipadas)', fontsize=16)
+plt.xlabel('Tempo (Meses)', fontsize=14)
+plt.ylabel('Patrimônio (R$)', fontsize=14)
+plt.legend(title='Tipo de Investimento', fontsize=12)
+plt.tight_layout()
+
+
+# Display do gráfico
+plt.show()
